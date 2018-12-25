65af4b76f20e6b8c88abca5caa991b1de786aeec
Anas <anas_imtiaz@yahoo.com>
2018-12-25 17:40:10 -0500

    Canvas fontSize incorrect for screens with different resolutions; resolved

--- js/drawCanvas.js
+++ js/drawCanvas.js
@@ -2,11 +2,8 @@ function drawCanvas(ratio)
 {
     WebFont.load({
         google: {
-          families: [/* 'Tulpen One', */
+          families: [
                      'Fredericka the Great',
-                    //  'Sirin Stencil',
-                    //  'Baumans',
-                    //  'Paprika',
                      'Cinzel Decorative'
                     ]
         },
@@ -15,13 +12,14 @@ function drawCanvas(ratio)
             var ctx = canvas.getContext("2d");
             var dpi = window.devicePixelRatio;
 
-            var fontSize = 150, offset = 20;
-
             var new_width = getComputedStyle(canvas).getPropertyValue('width').slice(0,-2),
                 new_height = getComputedStyle(canvas).getPropertyValue('height').slice(0,-2);
 
             canvas.setAttribute("width", new_width * dpi);
             canvas.setAttribute("height", new_height * dpi);
+
+            var ratio = 13;
+            var fontSize = canvas.getAttribute("width") / ratio, offset = fontSize / 7;
 
             ctx.font = fontSize + "px Fredericka the Great";
             ctx.textAlign = "center";
